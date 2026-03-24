33d0d04f7a45868f52422f56a3a44744fc29ddd5
oscurapooja <oscura.pooja@gmail.com>
2026-03-24 13:01:39 +0530

    settings.py

--- securevault/settings.py
+++ securevault/settings.py
@@ -91,6 +91,7 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'sbatman661@gmail.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'lkdv fcpb uwek hklf')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', 'http://localhost').split(',')
 # ── LOGIN LOCKOUT ──
 MAX_LOGIN_ATTEMPTS = 5
 LOCKOUT_DURATION_MINUTES = 15
