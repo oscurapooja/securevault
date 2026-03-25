88000b2f4f62f5e3cc42587a4d572aee1a67b38d
oscurapooja <oscura.pooja@gmail.com>
2026-03-25 07:48:25 +0530

    build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-echo '#!/usr/bin/env bash
+#!/usr/bin/env bash
 set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate' > build.sh
+python manage.py migrate
